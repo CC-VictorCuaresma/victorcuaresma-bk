f04c3923723c5a278f5311cd570a9a3c67b34c89
CC-VictorCuaresma <118938567+CC-VictorCuaresma@users.noreply.github.com>
2025-07-05 17:31:33 +0000

    todo ok

---
+++ src/test/java/pe/edu/vallegrande/victorcuaresma/VictorCuaresaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.victorcuaresma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VictorCuaresaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
