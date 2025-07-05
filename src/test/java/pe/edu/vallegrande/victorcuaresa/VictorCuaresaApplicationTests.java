4a253cbd850785640dac64ca5b5b2bbebcad984d
CC-VictorCuaresma <118938567+CC-VictorCuaresma@users.noreply.github.com>
2025-07-05 11:47:15 -0500

    Add files via upload

---
+++ src/test/java/pe/edu/vallegrande/victorcuaresa/VictorCuaresaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.victorcuaresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VictorCuaresaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
